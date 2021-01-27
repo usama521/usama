d37047c5b948eb6e473866afec0c221c8a328020
LinuxPlus <linuxplus@LinuxPluss-Mac.local>
2021-01-26 21:24:13 -0800

    Initial Commit

---
+++ start/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  start
+//
+//  Created by LinuxPlus on 1/26/21.
+//  Copyright © 2021 LinuxPlus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
